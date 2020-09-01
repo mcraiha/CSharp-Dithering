aec45fc8a7b6be4d4c1705a4f80a939bc38e4f6c
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2020-09-01 20:05:59 +0300

    Use byte[] based color function in benchmark
    I forgot this

--- benchmark/Program.cs
+++ benchmark/Program.cs
@@ -4,43 +4,43 @@ using BenchmarkDotNet.Running;
 
 namespace benchmark
 {
-    public class TempByteImageFormat1dvs3d
-    {
-        private const int dimension = 256;
-        private TempByteImageFormat oneD = new TempByteImageFormat(new byte[dimension * dimension * 3], dimension, dimension, 3);
+	public class TempByteImageFormat1dvs3d
+	{
+		private const int dimension = 256;
+		private TempByteImageFormat oneD = new TempByteImageFormat(new byte[dimension * dimension * 3], dimension, dimension, 3);
 
-        private TempByteImageFormat threeD = new TempByteImageFormat(new byte[dimension, dimension, 3]);
+		private TempByteImageFormat threeD = new TempByteImageFormat(new byte[dimension, dimension, 3]);
 
-        FakeDitheringRGBByte fake = new FakeDitheringRGBByte(TrueColorBytesToWebSafeColorBytes);
+		FakeDitheringRGBByte fake = new FakeDitheringRGBByte(TrueColorBytesToWebSafeColorBytes);
 
-        public TempByteImageFormat1dvs3d()
-        {
-            
-        }
+		public TempByteImageFormat1dvs3d()
+		{
+			
+		}
 
-        [Benchmark]
-        public void onedee() => fake.DoDithering(oneD);
+		[Benchmark]
+		public void onedee() => fake.DoDithering(oneD);
 
-        [Benchmark]
-        public void threedee() => fake.DoDithering(threeD);
+		[Benchmark]
+		public void threedee() => fake.DoDithering(threeD);
 
-        private static object[] TrueColorBytesToWebSafeColorBytes(object[] input)
-        {
-            object[] returnArray = new object[input.Length];
-            for (int i = 0; i < returnArray.Length; i++)
-            {
-                returnArray[i] = (byte)(Math.Round((byte)input[i] / 51.0) * 51);
-            }
-            
-            return returnArray;
-        }
-    }
+		private static byte[] TrueColorBytesToWebSafeColorBytes(byte[] input)
+		{
+			byte[] returnArray = new byte[input.Length];
+			for (int i = 0; i < returnArray.Length; i++)
+			{
+				returnArray[i] = (byte)(Math.Round(input[i] / 51.0) * 51);
+			}
+			
+			return returnArray;
+		}
+	}
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var summary = BenchmarkRunner.Run<TempByteImageFormat1dvs3d>();
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var summary = BenchmarkRunner.Run<TempByteImageFormat1dvs3d>();
+		}
+	}
 }
